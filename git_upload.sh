1cccaddc12846c85b5723c44c9c8245622dff12d
ethan zhou <zhou_yc@Hotmail.com>
2018-12-03 23:25:16 -0800

    support new ADDED files and files both working and stageing area

--- git_upload.sh
+++ git_upload.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-target_dir=/geemusic
-file_list=$(git status -s|grep -w M|cut -d' ' -f3)
+target_dir=/geemusic_my_fork
+#file_list=$(git status -s|grep -w M|cut -d' ' -f3)
+file_list=$(git status -s|grep -E -w 'A|M|MM'|awk '{print $NF}')
+
+
 #echo $file_list
 for i in $file_list
 do
     if [ "$i" != "Dockerfile" ]; then
         echo "UPLOADING file to docker:" $i
         #echo "target_file=" $target_dir/$i
-        docker cp $i f596b2628559:$target_dir/$i
+        docker cp $i test_gee_portmapping:$target_dir/$i
 	fi
 done
